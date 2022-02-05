0301eecad94966557a98a8f0fb264bb7811dc14d
Spencer <spencer.bigum@gmail.com>
2022-02-05 13:37:42 -0500

    fixed gumroad btn link 6

---
+++ app/components/buttons/gumroadBtn.tsx
@@ -0,0 +1,17 @@
+import { formatePrice } from '~/utils/priceUtils'
+
+interface Props {
+  price: number
+}
+
+function GumroadBtn(props: Props) {
+  const { price } = props
+
+  return (
+    <button className="btn btn-teal font-normal flex justify-between text-lg px-5">
+      <span>Buy Now</span>
+      <span className="font-sentinel__SemiBoldItal text-2xl">{formatePrice(price, true)}</span>
+    </button>
+  )
+}
+export default GumroadBtn
