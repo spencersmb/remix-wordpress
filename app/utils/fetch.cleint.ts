908e830581becba345275176cd6ae805f2858a41
Spencer <spencer.bigum@gmail.com>
2022-07-05 01:35:23 -0400

    hostname test

--- app/utils/fetch.cleint.ts
+++ app/utils/fetch.cleint.ts
@@ -135,10 +135,14 @@ const fetchSubmitComment: FetchSubmitCommentType = (comment) => {
 Font File fetch
 */
 export async function fetchFontPreviewFile(fontName: string){
-  const rootUrl = window.ENV.APP_ROOT_URL as string
+  // const rootUrl = window.ENV.APP_ROOT_URL as string
+  const rootUrl = window.location.origin
   const url = `${rootUrl}/api/font/file?font=${fontName}`;
-
+  const hostname = window.location.protocol + "//" 
+    + window.location.hostname
   console.log('fontUrl', url);
+  console.log('host', hostname);
+  
   
   const res = await fetch(url, {
     method: 'GET',
