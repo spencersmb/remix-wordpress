4c5c63f0787d4896802cb72340918d7ad0429b2a
Spencer <spencer.bigum@gmail.com>
2023-04-02 07:57:24 -0400

    added sanity test

---
+++ app/routes/sanity.tsx
@@ -0,0 +1,20 @@
+import Layout from "@App/components/layoutTemplates/layout";
+import { client } from "@App/lib/sanity/sanity";
+import { useLoaderData } from "@remix-run/react";
+
+export const loader = async () => {
+  const query = `*[_type == "post"]`;
+  const posts = await client.fetch(query);
+
+  return { posts };
+};
+
+export default function Sanity() {
+  const { posts } = useLoaderData();
+  console.log('posts', posts)
+  return (
+    <Layout>
+      <h1 className="h-[500px]">Posts</h1>
+    </Layout>
+  )
+}
